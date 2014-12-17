37e195d7c286fc5ce64afa4f9d98e54e502b0b42
Maxim Kochetkov <fido_max@inbox.ru>
2014-12-17 16:22:18 +0300

    + add fastwell DAQ module

---
+++ src/moduls/daq/Fastwell/module.cpp
@@ -0,0 +1,340 @@
+
+//OpenSCADA system module DAQ.Fastwell file: module.cpp
+/***************************************************************************
+ *   Copyright (C) 2014 by Maxim Kochetkov                                 *
+ *   fido_max@inbox.ru                                                     *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; version 2 of the License.               *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include <unistd.h>
+#include <signal.h>
+#include <fcntl.h>
+
+#include <terror.h>
+#include <tsys.h>
+#include <tmess.h>
+#include <ttiparam.h>
+#include <tdaqs.h>
+
+#include "module.h"
+
+//*************************************************
+//* Module info!                                  *
+#define MOD_ID		"Fastwell"
+#define MOD_NAME	_("Fastwell IO")
+#define MOD_TYPE	SDAQ_ID
+#define VER_TYPE	SDAQ_VER
+#define MOD_VER		"0.0.1"
+#define AUTHORS		_("Maxim Kochetkov")
+#define DESCRIPTION	_("Fastwell IO FBUS client implementation")
+#define LICENSE		"GPL2"
+//*************************************************
+
+ModFastwell::TTpContr *ModFastwell::mod;  //Pointer for direct access to the module
+
+//!!! Required section for binding OpenSCADA core to this module. It provides information and create module root object.
+//!!! Do not remove this section!
+extern "C"
+{
+#ifdef MOD_INCL
+    TModule::SAt daq_Fastwell_module( int n_mod )
+#else
+    TModule::SAt module( int n_mod )
+#endif
+    {
+	if(n_mod == 0)	return TModule::SAt(MOD_ID, MOD_TYPE, VER_TYPE);
+	return TModule::SAt("");
+    }
+
+#ifdef MOD_INCL
+    TModule *daq_Fastwell_attach( const TModule::SAt &AtMod, const string &source )
+#else
+    TModule *attach( const TModule::SAt &AtMod, const string &source )
+#endif
+    {
+	if(AtMod == TModule::SAt(MOD_ID,MOD_TYPE,VER_TYPE))
+	    return new ModFastwell::TTpContr(source);
+	return NULL;
+    }
+}
+
+using namespace ModFastwell;
+
+//*************************************************
+//* TTpContr                                      *
+//*************************************************
+TTpContr::TTpContr( string name ) : TTipDAQ(MOD_ID)
+{
+    mod		= this;
+
+    mName	= MOD_NAME;
+    mType	= MOD_TYPE;
+    mVers	= MOD_VER;
+    mAuthor	= AUTHORS;
+    mDescr	= DESCRIPTION;
+    mLicense	= LICENSE;
+    mSource	= name;
+}
+
+TTpContr::~TTpContr( )
+{
+
+}
+
+string TTpContr::optDescr( )
+{
+    char buf[STR_BUF_LEN];
+
+    snprintf(buf,sizeof(buf),_(
+	"======================= The module <%s:%s> options =======================\n"
+	"---------- Parameters of the module section '%s' in config-file ----------\n\n"),
+	MOD_TYPE,MOD_ID,nodePath().c_str());
+
+    return buf;
+}
+
+void TTpContr::load_( )
+{
+    //> Load parameters from command line
+    string argCom, argVl;
+    for(int argPos = 0; (argCom=SYS->getCmdOpt(argPos,&argVl)).size(); )
+        if(argCom == "h" || argCom == "help")	fprintf(stdout, "%s", optDescr().c_str());
+}
+
+void TTpContr::save_( )
+{
+
+}
+
+void TTpContr::postEnable( int flag )
+{
+    TTipDAQ::postEnable(flag);
+
+    //> Controler's bd structure
+    fldAdd(new TFld("PRM_BD",_("Parameteres table"),TFld::String,TFld::NoFlag,"30",""));
+    fldAdd(new TFld("SCHEDULE",_("Acquisition schedule"),TFld::String,TFld::NoFlag,"100","1"));
+    fldAdd(new TFld("PRIOR",_("Gather task priority"),TFld::Integer,TFld::NoFlag,"2","0","-1;99"));
+
+    //> Parameter type bd structure
+    int t_prm = tpParmAdd("std", "PRM_BD", _("Standard"));
+    tpPrmAt(t_prm).fldAdd(new TFld("OID_LS",_("OID list (next line separated)"),TFld::String,TFld::FullText|TCfg::NoVal,"100",""));
+}
+
+TController *TTpContr::ContrAttach( const string &name, const string &daq_db )
+{
+    return new TMdContr(name, daq_db, this);
+}
+
+//*************************************************
+//* TMdContr                                      *
+//*************************************************
+TMdContr::TMdContr( string name_c, const string &daq_db, ::TElem *cfgelem ) :
+    ::TController(name_c,daq_db,cfgelem), prcSt(false), callSt(false), endrunReq(false), tmGath(0),
+    mSched(cfg("SCHEDULE")), mPrior(cfg("PRIOR"))
+{
+    cfg("PRM_BD").setS("TmplPrm_"+name_c);
+}
+
+TMdContr::~TMdContr( )
+{
+    if(startStat()) stop();
+}
+
+string TMdContr::getStatus( )
+{
+    string rez = TController::getStatus();
+    if(startStat() && !redntUse())
+    {
+	if(callSt)	rez += TSYS::strMess(_("Call now. "));
+	if(period())	rez += TSYS::strMess(_("Call by period: %s. "), tm2s(1e-3*period()).c_str());
+	else rez += TSYS::strMess(_("Call next by cron '%s'. "), tm2s(TSYS::cron(cron()),"%d-%m-%Y %R").c_str());
+	rez += TSYS::strMess(_("Spent time: %s."), tm2s(tmGath).c_str());
+    }
+    return rez;
+}
+
+TParamContr *TMdContr::ParamAttach( const string &name, int type )
+{
+    return new TMdPrm(name, &owner().tpPrmAt(type));
+}
+
+void TMdContr::start_( )
+{
+    //> Schedule process
+    mPer = TSYS::strSepParse(cron(),1,' ').empty() ? vmax(0,(int64_t)(1e9*atof(cron().c_str()))) : 0;
+
+    //> Start the gathering data task
+    SYS->taskCreate(nodePath('.',true), mPrior, TMdContr::Task, this);
+}
+
+void TMdContr::stop_( )
+{
+    //> Stop the request and calc data task
+    SYS->taskDestroy(nodePath('.',true), &endrunReq);
+}
+
+void TMdContr::prmEn( const string &id, bool val )
+{
+    int i_prm;
+
+    ResAlloc res(en_res, true);
+    for(i_prm = 0; i_prm < p_hd.size(); i_prm++)
+	if(p_hd[i_prm].at().id() == id) break;
+
+    if(val && i_prm >= p_hd.size())	p_hd.push_back(at(id));
+    if(!val && i_prm < p_hd.size())	p_hd.erase(p_hd.begin()+i_prm);
+}
+
+void *TMdContr::Task( void *icntr )
+{
+    TMdContr &cntr = *(TMdContr *)icntr;
+
+    cntr.endrunReq = false;
+    cntr.prcSt = true;
+
+    while(!cntr.endrunReq)
+    {
+	int64_t t_cnt = TSYS::curTime();
+
+	//> Update controller's data
+	//!!! Your code for gather data
+	cntr.en_res.resRequestR( );
+	cntr.callSt = true;
+	for(unsigned i_p = 0; i_p < cntr.p_hd.size() && !cntr.redntUse(); i_p++)
+	    try
+	    {
+		//!!! Process parameter code
+	    }
+	    catch(TError err)
+	    { mess_err(err.cat.c_str(), "%s", err.mess.c_str()); }
+	cntr.callSt = false;
+	cntr.en_res.resRelease();
+	cntr.tmGath = TSYS::curTime()-t_cnt;
+
+	//!!! Wait for next iteration
+	TSYS::taskSleep(cntr.period(), (cntr.period()?0:TSYS::cron(cntr.cron())));
+    }
+
+    cntr.prcSt = false;
+
+    return NULL;
+}
+
+void TMdContr::cntrCmdProc( XMLNode *opt )
+{
+    //> Get page info
+    if(opt->name() == "info")
+    {
+	TController::cntrCmdProc(opt);
+	ctrMkNode("fld",opt,-1,"/cntr/cfg/SCHEDULE",cfg("SCHEDULE").fld().descr(),startStat()?R_R_R_:RWRWR_,"root",SDAQ_ID,3,
+	    "dest","sel_ed","sel_list",TMess::labSecCRONsel(),"help",TMess::labSecCRON());
+	ctrMkNode("fld",opt,-1,"/cntr/cfg/PRIOR",cfg("PRIOR").fld().descr(),startStat()?R_R_R_:RWRWR_,"root",SDAQ_ID,1,"help",TMess::labTaskPrior());
+	return;
+    }
+    //> Process command to page
+    TController::cntrCmdProc(opt);
+}
+
+//*************************************************
+//* TMdPrm                                        *
+//*************************************************
+TMdPrm::TMdPrm( string name, TTipParam *tp_prm ) :
+    TParamContr(name,tp_prm), p_el("w_attr")
+{
+
+}
+
+TMdPrm::~TMdPrm( )
+{
+    //!!! Call for prevent access to data the object from included nodes on destruction.
+    nodeDelAll();
+}
+
+void TMdPrm::postEnable( int flag )
+{
+    TParamContr::postEnable(flag);
+    if(!vlElemPresent(&p_el))   vlElemAtt(&p_el);
+}
+
+TMdContr &TMdPrm::owner( )	{ return (TMdContr&)TParamContr::owner(); }
+
+void TMdPrm::enable( )
+{
+    if(enableStat())	return;
+
+    TParamContr::enable();
+
+    owner().prmEn(id(), true);
+}
+
+void TMdPrm::disable( )
+{
+    if(!enableStat())  return;
+
+    owner().prmEn(id(), false);
+
+    TParamContr::disable();
+
+    //> Set EVAL to parameter attributes
+    vector<string> ls;
+    elem().fldList(ls);
+    for(int i_el = 0; i_el < ls.size(); i_el++)
+	vlAt(ls[i_el]).at().setS(EVAL_STR, 0, true);
+}
+
+void TMdPrm::load_( )
+{
+    TParamContr::load_();
+}
+
+void TMdPrm::save_( )
+{
+    TParamContr::save_();
+}
+
+void TMdPrm::cntrCmdProc( XMLNode *opt )
+{
+    //> Service commands process
+    string a_path = opt->attr("path");
+    if(a_path.substr(0,6) == "/serv/")	{ TParamContr::cntrCmdProc(opt); return; }
+
+    //> Get page info
+    if(opt->name() == "info")
+    {
+	TParamContr::cntrCmdProc(opt);
+	ctrMkNode("fld",opt,-1,"/prm/cfg/OID_LS",cfg("OID_LS").fld().descr(),enableStat()?R_R_R_:RWRWR_,"root",SDAQ_ID);
+	return;
+    }
+
+    //> Process command to page
+    if(a_path == "/prm/cfg/OID_LS" && ctrChkNode(opt,"set",RWRWR_,"root",SDAQ_ID,SEC_WR))
+    {
+	if(enableStat()) throw TError(nodePath().c_str(),"Parameter is enabled.");
+//	parseOIDList(opt->text());
+    }
+    else TParamContr::cntrCmdProc(opt);
+}
+
+void TMdPrm::vlArchMake( TVal &val )
+{
+    TParamContr::vlArchMake(val);
+
+    if(val.arch().freeStat()) return;
+    val.arch().at().setSrcMode(TVArchive::PassiveAttr);
+    val.arch().at().setPeriod((int64_t)(owner().period()*1000000));
+    val.arch().at().setHardGrid(true);
+    val.arch().at().setHighResTm(true);
+}
