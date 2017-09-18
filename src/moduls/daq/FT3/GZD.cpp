1543424c8c8a6e903c9126d1e7a27f7e3ae9a381
Maxim Kochetkov <fido_max@inbox.ru>
2017-09-18 07:56:41 +0300

    * cleanup

--- src/moduls/daq/FT3/GZD.cpp
+++ src/moduls/daq/FT3/GZD.cpp
@@ -151,9 +151,8 @@ uint8_t KA_GZD::cmdGet(uint16_t prmID, uint8_t * out)
 	FT3ID ft3ID = UnpackID(prmID);
 	FT3ID ft3ID_ZD = UnpackID(prmID);
 
-//    if(mess_lev() == TMess::Debug) mPrm.mess_sys(TMess::Debug, "ID %d ft3ID g%d k%d n%d ", ID, ft3ID.g, ft3ID.k, ft3ID.n);
 	if (ft3ID.g != ID) return 0;
-	uint l = 0;
+	uint8_t l = 0;
 	uint8_t ll = 0;
 	if (ft3ID.k == 0) {
 		switch (ft3ID.n) {
@@ -168,10 +167,6 @@ uint8_t KA_GZD::cmdGet(uint16_t prmID, uint8_t * out)
 			l = 2;
 			break;
 		case 2:
-			/*for (uint8_t i = 0; i < count_n; i++) {
-			    out[i * 2] = i;
-			    out[i * 2 + 1] = data[i].State.vl;
-			   }*/
 			for (uint8_t i = 1; i <= count_n; i++) {
 				ll = 0;
 				ft3ID_ZD.k = i;
@@ -190,8 +185,6 @@ uint8_t KA_GZD::cmdGet(uint16_t prmID, uint8_t * out)
 					break;
 			}
 			if (l != count_n * 2) l = 0;
-
-			l = count_n * 2;
 			break;
 		}
 	}
@@ -214,26 +207,26 @@ uint16_t KA_GZD::setVal(TVal &val)
 }
 
 KA_ZD::KA_ZD(TMdPrm& prm, DA &parent, uint16_t id, bool has_params, uint32_t v_type) :
-	DA(prm, id), parentDA(parent), with_params(has_params), valve_type(v_type),// da(owner), id(iid),
-	State(TSYS::strMess("state_%d", id), TSYS::strMess(_("State %d"), id)),
-	Function(TSYS::strMess("function_%d", id), TSYS::strMess(_("Function %d"), id)),
-	TUOpen(TSYS::strMess("TUopen_%d", id), TSYS::strMess(_("TU open %d"), id)),
-	TUClose(TSYS::strMess("TUclose_%d", id), TSYS::strMess(_("TU close %d"), id)),
-	TUStop(TSYS::strMess("TUstop_%d", id), TSYS::strMess(_("TU stop %d"), id)),
-	TURemote(TSYS::strMess("TUremote_%d", id), TSYS::strMess(_("TU remote %d"), id)),
-	TUManual(TSYS::strMess("TUmanual_%d", id), TSYS::strMess(_("TU manual %d"), id)),
-	TUStopEx(TSYS::strMess("TUstopEx_%d", id), TSYS::strMess(_("TU stop Ex %d"), id)),
-	TimeOpen(TSYS::strMess("timeOpen_%d", id), TSYS::strMess(_("Open time %d"), id)),
-	TimeClose(TSYS::strMess("timeClose_%d", id), TSYS::strMess(_("Close time %d"), id)),
-	TimeStop(TSYS::strMess("timeStop_%d", id), TSYS::strMess(_("Stop time %d"), id)),
-	TimeStopEx(TSYS::strMess("timeStopEx_%d", id), TSYS::strMess(_("Stop time Ex %d"), id)),
-	TimeRemote(TSYS::strMess("timeRemote_%d", id), TSYS::strMess(_("Remote time %d"), id)),
-	TimeManual(TSYS::strMess("timeManual_%d", id), TSYS::strMess(_("Manual time %d"), id)),
-	TCOpen(TSYS::strMess("tcOpen_%d", id), TSYS::strMess(_("Open TC %d"), id)),
-	TCClose(TSYS::strMess("tcClose_%d", id), TSYS::strMess(_("Close TC %d"), id)),
-	TCMode(TSYS::strMess("tcMode_%d", id), TSYS::strMess(_("Mode TC %d"), id)),
-	TCOpenErr(TSYS::strMess("tcOpenErr_%d", id), TSYS::strMess(_("Open error TC %d"), id)),
-	TCCloseErr(TSYS::strMess("tcCloseErr_%d", id), TSYS::strMess(_("Close error TC %d"), id))
+	DA(prm, id), parentDA(parent), with_params(has_params), valve_type(v_type),
+	State(TSYS::strMess("state", id), TSYS::strMess(_("State"), id)),
+	Function(TSYS::strMess("function", id), TSYS::strMess(_("Function"), id)),
+	TUOpen(TSYS::strMess("TUopen", id), TSYS::strMess(_("TU open"), id)),
+	TUClose(TSYS::strMess("TUclose", id), TSYS::strMess(_("TU close"), id)),
+	TUStop(TSYS::strMess("TUstop", id), TSYS::strMess(_("TU stop"), id)),
+	TURemote(TSYS::strMess("TUremote", id), TSYS::strMess(_("TU remote"), id)),
+	TUManual(TSYS::strMess("TUmanual", id), TSYS::strMess(_("TU manual"), id)),
+	TUStopEx(TSYS::strMess("TUstopEx", id), TSYS::strMess(_("TU stop Ex"), id)),
+	TimeOpen(TSYS::strMess("timeOpen", id), TSYS::strMess(_("Open time"), id)),
+	TimeClose(TSYS::strMess("timeClose", id), TSYS::strMess(_("Close time"), id)),
+	TimeStop(TSYS::strMess("timeStop", id), TSYS::strMess(_("Stop time"), id)),
+	TimeStopEx(TSYS::strMess("timeStopEx", id), TSYS::strMess(_("Stop time Ex"), id)),
+	TimeRemote(TSYS::strMess("timeRemote", id), TSYS::strMess(_("Remote time"), id)),
+	TimeManual(TSYS::strMess("timeManual", id), TSYS::strMess(_("Manual time"), id)),
+	TCOpen(TSYS::strMess("tcOpen", id), TSYS::strMess(_("Open TC"), id)),
+	TCClose(TSYS::strMess("tcClose", id), TSYS::strMess(_("Close TC"), id)),
+	TCMode(TSYS::strMess("tcMode", id), TSYS::strMess(_("Mode TC"), id)),
+	TCOpenErr(TSYS::strMess("tcOpenErr", id), TSYS::strMess(_("Open error TC"), id)),
+	TCCloseErr(TSYS::strMess("tcCloseErr", id), TSYS::strMess(_("Close error TC"), id))
 {
 	mTypeFT3 = KA;
 
@@ -754,7 +747,6 @@ uint8_t KA_ZD::cmdSet(uint8_t * req, uint8_t addr)
 	if (ft3ID.g != parentDA.ID) return 0;
 	if (ft3ID.k != ID) return 0;
 	uint8_t l = 0;
-
 	switch (ft3ID.n) {
 	case 0:
 		l = SetNewState(addr, prmID, req + 2);
@@ -769,7 +761,7 @@ uint8_t KA_ZD::cmdSet(uint8_t * req, uint8_t addr)
 		l = SetNewFunction(addr, prmID, req + 2);
 		break;
 	}
-
+    mPrm.mess_sys(TMess::Error, "cmdSet g %d k %d n %d %d", ft3ID.g, ft3ID.k, ft3ID.n, l);
 	return l;
 }
 
@@ -778,32 +770,6 @@ uint16_t KA_ZD::setVal(TVal &val)
 	uint16_t rc = 0;
 	int off = 0;
 	FT3ID ft3ID;
-
-/*	ft3ID.k = ID;
-	ft3ID.n = s2i(val.fld().reserve());
-	ft3ID.g = parentDA.ID;
-
-	tagMsg Msg;
-	Msg.L = 0;
-	Msg.C = SetData;
-	Msg.L += SerializeUi16(Msg.D + Msg.L, PackID(ft3ID));
-	switch (ft3ID.n) {
-	case 0:
-		Msg.L += Value.SerializeAttr(Msg.D + Msg.L);
-		rc = 1;
-		break;
-	case 1:
-		Msg.L += Period.SerializeAttr(Msg.D + Msg.L);
-		Msg.L += Count.SerializeAttr(Msg.D + Msg.L);
-		rc = 1;
-		break;
-	}
-	if (Msg.L > 2) {
-		Msg.L += 3;
-		mPrm.owner().DoCmd(&Msg);
-	}
-
-	return rc;*/
 
 	ft3ID.k = ID;
 	ft3ID.n = s2i(val.fld().reserve());
@@ -844,7 +810,8 @@ uint16_t KA_ZD::setVal(TVal &val)
 		rc = 1;
 		break;
 	case 3:
-		Msg.L += SerializeB(Msg.D + Msg.L, val.getI(0, true));
+		Msg.L += Function.SerializeAttr(Msg.D + Msg.L);
+		rc = 1;
 		break;
 	}
 	if (Msg.L > 2) {
